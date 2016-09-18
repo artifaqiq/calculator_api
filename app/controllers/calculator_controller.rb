8f5e28282a1b8e66edbea8f2fc34e8b91b84e52c
Artur Lomako <artifaqiq@gmail.com>
2016-09-18 23:55:29 +0300

    v1.0.0

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -2,23 +2,32 @@ class CalculatorController < ApplicationController
   def show
 
     if !request.parameters.key? 'expression'
-      result = 'no parameters \'expression\''
+      result = 'ClientApplicationError: no parameters \'expression\''
+      status = 'error'
       puts("no parameters expression")
     elsif !!(request.parameters['expression'] =~ /^[^a-zA-z]+$/) == false
-      result = 'incorrect expression'
+      result = 'Syntax error: incorrect expression'
+      status = 'error'
       puts("bad expression (regexp)")
     else
       begin
         result = eval(request.parameters['expression'])
       rescue SyntaxError
-        result = 'incorrect expression'
+        result = 'Syntax error: incorrect expression'
+        status = 'error'
+      rescue ZeroDivisionError
+          result = 'Arithmetic error: division by zero'
+          status = 'error'
+      else
+        status = 'ok'
       end
     end
 
     puts result
 
     render json: {
-        'result'     => result
+        'result'     => result.to_s,
+        'status'     => status
     }
 
   end
